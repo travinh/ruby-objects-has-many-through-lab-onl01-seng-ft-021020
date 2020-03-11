fec98f9ec52dcc82cda6604e49d2e1c82fe7beaf
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-11 03:10:56 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+require_relative "./song.rb"
+
+class Genre
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self}
+  end
+
+  def artists
+    songs.map { |song| song.artist }
+  end
+
+  def self.all
+    @@all
+  end
+end
